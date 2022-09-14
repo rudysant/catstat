8cedc1c04fe2fbf53c65c50134891e334f96c463
rudysant <rudysant@gmail.com>
2022-09-14 14:05:03 +1000

    "Add models Size as ForeignKey for Catalogues"

--- admin.py
+++ admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from . models import Catalogues
+from . models import Catalogues, Size
 
 class CataloguesA(admin.ModelAdmin):
     list_display = ('fulltitle','consnumber')
+    
+class SizeA(admin.ModelAdmin):
+    list_display = ('id', 'booksize')
 
 admin.site.register(Catalogues, CataloguesA)
+admin.site.register(Size, SizeA)
 
 # Register your models here.
--- models.py
+++ models.py
@@ -2,6 +2,13 @@ from optparse import Option
 from django.db import models
 
 # Create your models here.
+
+class Size(models.Model):
+    booksize = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return f"{self.booksize}"
+    
 class Catalogues(models.Model):
     entrydate = models.DateField()
     consnumber = models.CharField(max_length=20, default='461/')
@@ -13,6 +20,10 @@ class Catalogues(models.Model):
     publishertype = models.CharField(max_length=100, choices=[('Commercial','Commercial'),('Government-department','Government-department'),('Government-nondepartment','Government-nondepartment'),('Government-local','Government-local'),('Universities','Universities'),('NGO','NGO'),('Others','Others')], default='Commercial')
     genre = models.CharField(max_length=100, choices=[('General','General'),('Literature','Literature'),('Biography','Biography')], default='General')
     pubformat = models.CharField(max_length=100, choices=[('Book','Book'),('Non book','Non book')], default='Book')
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.fulltitle} == {self.consnumber}"
+        
+
+    
